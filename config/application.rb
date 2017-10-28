f29a17696b768bce603fff0b97f9e04e7ffc666c
Natthakit <nonusae@gmail.com>
2017-10-28 15:15:42 +0700

    Prodject setup with rspec config

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+Bundler.require(*Rails.groups)
+
+module Projects
+  class Application < Rails::Application
+    config.load_defaults 5.1
+    config.generators do |g| 
+        g.test_framework :rspec,
+            fixtures: false, 
+            view_specs: false, 
+            helper_specs: false, 
+            routing_specs: false
+    end
+  end
+end
