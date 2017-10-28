d7da70ad8395c538fd21dba9c120e91c2881a44b
Natthakit <nonusae@gmail.com>
2017-10-29 03:26:51 +0700

    Integrated Capybara test and selenium to make basic feature test

--- Gemfile
+++ Gemfile
@@ -16,10 +16,14 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'capybara', '~> 2.13.0'
   gem 'selenium-webdriver'
   gem 'rspec-rails', '~> 3.6.0'
   gem 'factory_girl_rails', "~> 4.8.0"
+end
+
+group :test do
+    gem 'capybara', '~> 2.13.0'
+    gem 'launchy', '~> 2.4.3'
 end
 
 group :development do
---
+++ spec/support/capybara.rb
@@ -0,0 +1,17 @@
+require "selenium/webdriver"
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
