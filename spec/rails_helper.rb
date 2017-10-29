8b6bef944ad41686a17d3eec35889f2ace528992
Natthakit <nonusae@gmail.com>
2017-10-29 21:34:25 +0700

    Finish first specfor paper clip and resync git history with gitignore :)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.after(:suite) do 
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_uploads/"])
+end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
